ebad5ebd93334bab581a9cafccfd1a5f095579c5
Wellynton Delfino <wnodelfino@gmail.com>
2021-07-03 11:00:43 -0300

    Inicializando repositório

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,37 @@
+import { startOfHour } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentRepository: AppointmentsRepository;
+
+  constructor(appointmentRepository: AppointmentsRepository) {
+    this.appointmentRepository = appointmentRepository;
+  }
+
+  public execute({ date, provider }: Request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate =
+      this.appointmentRepository.findByDate(appointmentDate);
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = this.appointmentRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
