436443e43a38f1efcd62448903638bc5ff61fac4
Wellynton Delfino <wnodelfino@gmail.com>
2021-07-13 17:17:43 -0300

    Configurando Imports

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,11 +1,9 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
-import authConfig from '../../../config/auth';
-
-import AppError from '../../../errors/AppError';
-
-import User from '../entities/User';
+import AppError from '@shared/errors/AppError';
+import User from '@modules/users/infra/typeorm/entities/User';
+import authConfig from '@config/auth';
 
 interface Request {
   email: string;
