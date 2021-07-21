eb547c62788330a39faa92f09c1372beb37817f4
Wellynton Delfino <wnodelfino@gmail.com>
2021-07-21 11:03:53 -0300

    Recuperação de senha

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,10 +1,9 @@
 import { injectable, inject } from 'tsyringe';
 
-// import AppError from '@shared/errors/AppError';
-
-// import User from '../infra/typeorm/entities/User';
+import AppError from '@shared/errors/AppError';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
 interface IRequest {
   email: string;
@@ -18,9 +17,20 @@ class SendForgotPasswordEmailService {
 
     @inject('MailProvider')
     private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exixts');
+    }
+
+    await this.userTokensRepository.generate(user.id);
+
     await this.mailProvider.sendMail(
       email,
       'Pedido de recuperação de senha recebido',
